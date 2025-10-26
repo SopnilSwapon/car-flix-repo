cdd54db4afc6af2ce08360821677b02ef797549a
SopnilSwapon <sopnilswapon1@gmail.com>
2025-10-26 12:12:31 +0600

    added dashboard page

---
+++ app/Components/StateCard.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React from "react";
+import { TrendingUp } from "lucide-react";
+import Image, { StaticImageData } from "next/image";
+
+interface IStatCard {
+  title: string;
+  value: string | number;
+  percent?: number;
+  period?: string; // e.g. "Last Month"
+  Icon: StaticImageData;
+}
+
+const StatCard: React.FC<IStatCard> = ({
+  title,
+  value,
+  percent,
+  period,
+  Icon,
+}) => {
+  return (
+    <div className="w-full rounded-2xl border border-gray-200 bg-white p-5 shadow-sm flex flex-col justify-between">
+      {/* Top Section */}
+      <div className="flex items-center gap-2">
+        <div className="p-2 rounded-full bg-indigo-50 text-indigo-600">
+          <Image src={Icon} height={20} width={20} alt="user logo"/>
+        </div>
+        <h3 className="font-medium text-[18px]">{title}</h3>
+      </div>
+
+      <div className="flex justify-between pt-6">
+      <div className="text-3xl font-semibold font-roboto">{value}</div>
+
+      {/* Bottom Stats */}
+      <div className="text-sm flex justify-start items-end flex-col w-full">
+        <div className="flex gap-2 mb-1">
+
+  <TrendingUp className="text-[#4CAF50]" /> <span className="text-[16px]">6 %</span>
+        </div>
+        {period && <p className="text-gray-500">{period}</p>}
+      </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default StatCard;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,38 @@
+"use client"
 import React from 'react'
-
+import StatCard from '../Components/StateCard'
+import logo from "@/public/Frame (1).png"
 export default function page() {
   return (
-    <div className='text-red-800'>page</div>
+    <div className='py-6 mx-6 flex gap-4'>
+       <StatCard
+        title="Total subscription"
+        value={20}
+        percent={6}
+        period="Last Month"
+        Icon={logo}
+      />
+       <StatCard
+        title="On Going Work"
+        value={20}
+        percent={6}
+        period="Last Month"
+        Icon={logo}
+      />
+       <StatCard
+        title="Revenue"
+        value={20}
+        percent={6}
+        period="Last Month"
+        Icon={logo}
+      />
+       <StatCard
+        title="Social Request"
+        value={20}
+        percent={6}
+        period="Last Month"
+        Icon={logo}
+      />
+    </div>
   )
 }
